c308b9e283000cc7ec72b9c37a088c32ce076244
Shedriq <jerrydwain4@gmail.com>
2022-11-08 16:32:53 +0300

    update

--- 101-cocktail_sort_list.c
+++ 101-cocktail_sort_list.c
@@ -1,69 +1,78 @@
 #include "sort.h"
-#include <stdio.h>
 
 /**
- * swap - swaps a node with the next node in the list
- * @list: double pointer to the beginning of the list
+ * swap_nodes_cocktail - swaps two nodes in a doubly linked list
+ * @list: double pointer to linked list
  * @node: node to swap
  *
  * Return: void
  */
-void swap(listint_t **list, listint_t *node)
+void swap_nodes_cocktail(listint_t **list, listint_t *node)
 {
 	node->next->prev = node->prev;
 	if (node->prev)
+	{
 		node->prev->next = node->next;
+	}
 	else
+	{
 		*list = node->next;
+	}
 	node->prev = node->next;
 	node->next = node->next->next;
 	node->prev->next = node;
 	if (node->next)
+	{
 		node->next->prev = node;
+	}
 }
 
 /**
- * cocktail_sort_list - sorts a doubly linked list of integers in ascending
- * order using the Cocktail shaker sort algorithm
- * @list: Double pointer to the head of the doubly linked list
+ * cocktail_sort_list - sorts a doubly linked list of ints in ascending order
+ * @list: list to be sorted
  *
  * Return: void
  */
 void cocktail_sort_list(listint_t **list)
 {
-	char swapped = 1;
-	listint_t *temp;
+	int swapped = 1;
+	listint_t *node = *list;
 
-	if (list == NULL || *list == NULL)
+	if (*list == NULL || list == NULL)
+	{
 		return;
-	temp = *list;
+	}
 	while (swapped != 0)
 	{
 		swapped = 0;
-		while (temp->next != NULL)
+		while (node->next)
 		{
-			if (temp->next->n < temp->n)
+			if (node->n > node->next->n)
 			{
-				swap(list, temp);
+				swap_nodes_cocktail(list, node);
 				swapped = 1;
 				print_list(*list);
 			}
 			else
-				temp = temp->next;
+			{
+				node = node->next;
+			}
 		}
 		if (swapped == 0)
 			break;
 		swapped = 0;
-		while (temp->prev != NULL)
+		while (node->prev)
 		{
-			if (temp->prev->n > temp->n)
+			if (node->n < node->prev->n)
 			{
-				swap(list, temp->prev);
+				swap_nodes_cocktail(list, node->prev);
 				swapped = 1;
 				print_list(*list);
 			}
 			else
-				temp = temp->prev;
+			{
+				node = node->prev;
+			}
 		}
 	}
 }
