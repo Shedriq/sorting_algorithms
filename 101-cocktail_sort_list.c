a608ddf5fd3f962faf6577d5fa189ce7554a4aa0
Shedriq <jerrydwain4@gmail.com>
2022-11-08 16:35:03 +0300

    update

--- 101-cocktail_sort_list.c
+++ 101-cocktail_sort_list.c
@@ -1,90 +1,78 @@
 #include "sort.h"
+
 /**
-*swap - swaps 2 nodes in a doubly-linked list
-*@a: address of first node
-*@b: address of second node
-*
-*Return: void
-*/
-void swap(listint_t *a, listint_t *b)
+ * swap_nodes_cocktail - swaps two nodes in a doubly linked list
+ * @list: double pointer to linked list
+ * @node: node to swap
+ *
+ * Return: void
+ */
+void swap_nodes_cocktail(listint_t **list, listint_t *node)
 {
-	if (a->prev)
-		a->prev->next = b;
-	if (b->next)
-		b->next->prev = a;
-	a->next = b->next;
-	b->prev = a->prev;
-	a->prev = b;
-	b->next = a;
-}
-/**
-*tail_traverse- function that sorts from the tail back
-*
-*@head: head of list
-*@tail: tail of the list
-*@list: original head of the list
-*
-*Return: new head of the list
-*/
-listint_t *tail_traverse(listint_t *head, listint_t *tail, listint_t *list)
-{
-	while (tail && tail->prev)
+	node->next->prev = node->prev;
+	if (node->prev)
 	{
-		if (tail->n < tail->prev->n)
-		{
-			swap(tail->prev, tail);
-			if (tail->prev == NULL)
-				list = tail;
-			print_list(list);
-		}
-		else
-			tail = tail->prev;
-		if (tail->prev == NULL)
-			head = tail;
+		node->prev->next = node->next;
 	}
-	return (head);
+	else
+	{
+		*list = node->next;
+	}
+	node->prev = node->next;
+	node->next = node->next->next;
+	node->prev->next = node;
+	if (node->next)
+	{
+		node->next->prev = node;
+	}
 }
 
 /**
-*cocktail_sort_list - sorts linked list using cocktail shaker sort
-*
-*@list: doubly linked list to be sorted
-*/
+ * cocktail_sort_list - sorts a doubly linked list of ints in ascending order
+ * @list: list to be sorted
+ *
+ * Return: void
+ */
 void cocktail_sort_list(listint_t **list)
 {
-	listint_t *tail, *head, *len;
-	int i = 0, j = 0, swaped = 1;
+	int swapped = 1;
+	listint_t *node = *list;
 
-	if (!list || !*list)
-		return;
-	len = *list;
-	for (i = 0; len; i++)
+	if (*list == NULL || list == NULL)
 	{
-		len = len->next;
+		return;
 	}
-	if (i < 2)
-		return;
-	head = *list;
-	while (j < i)
+	while (swapped != 0)
 	{
-		swaped = 0;
-		while (head && head->next)
+		swapped = 0;
+		while (node->next)
 		{
-			if (head->n > head->next->n)
+			if (node->n > node->next->n)
 			{
-				swap(head, head->next);
-				swaped++;
-				if (head->prev->prev == NULL)
-					*list = head->prev;
+				swap_nodes_cocktail(list, node);
+				swapped = 1;
 				print_list(*list);
 			}
 			else
-				head = head->next;
-			if (head->next == NULL)
-				tail = head;
+			{
+				node = node->next;
+			}
 		}
-		head = tail_traverse(head, tail, *list);
-		*list = head;
-		j++;
+		if (swapped == 0)
+			break;
+		swapped = 0;
+		while (node->prev)
+		{
+			if (node->n < node->prev->n)
+			{
+				swap_nodes_cocktail(list, node->prev);
+				swapped = 1;
+				print_list(*list);
+			}
+			else
+			{
+				node = node->prev;
+			}
+		}
 	}
 }
